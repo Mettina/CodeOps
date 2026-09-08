2ce04b33316dc651b701650302f626f02306482d
metin <metagesasebecs@gmail.com>
2026-09-08 11:59:35 +0300

    Exercise 1: install react-router-dom, wrap app in BrowserRouter with three routes

--- module3/day6/src/App.jsx
+++ module3/day6/src/App.jsx
@@ -1,16 +1,23 @@
+import { Routes, Route } from "react-router-dom";
 import Header from "./Header.jsx";
 import Menu from "./Menu.jsx";
 import OrderForm from "./OrderForm.jsx";
 import { CartProvider } from "./cart/CartProvider.jsx";
+import { ThemeProvider } from "./theme/ThemeContext.jsx";
 
 export default function App() {
   return (
-    <CartProvider>
-      <div className="app">
-        <Header />
-        <Menu />
-        <OrderForm />
-      </div>
-    </CartProvider>
+    <ThemeProvider>
+      <CartProvider>
+        <div className="app">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Menu />} />
+            <Route path="/menu" element={<Menu />} />
+            <Route path="/cart" element={<OrderForm />} />
+          </Routes>
+        </div>
+      </CartProvider>
+    </ThemeProvider>
   );
 }
